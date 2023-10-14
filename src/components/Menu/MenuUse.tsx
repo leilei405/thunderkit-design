7674429174d5e3afbb88dad4339f93d54356a4ca
fll <fllning@163.com>
2023-10-14 23:52:54 +0800

    feat: menu样式添加

--- src/components/Menu/MenuUse.tsx
+++ src/components/Menu/MenuUse.tsx
@@ -6,8 +6,15 @@ import MenuItem from "./menuItem";
 
 export const MenuUseCom: FC = () => {
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', marginTop: 100 }}>
-            <Menu defaultIndex={0} onSelect={(e) => console.log(e)}>
+        <div style={{ width: '80%', marginTop: '100px' }}>
+            <Menu style={{ float: 'left', marginLeft: 300 }} defaultIndex={0} onSelect={(e) => console.log(e)}>
+                <MenuItem index={0}>菜单1</MenuItem>
+                <MenuItem index={1}>菜单2</MenuItem>
+                <MenuItem index={2}>菜单3</MenuItem>
+                <MenuItem index={3}>菜单4</MenuItem>
+            </Menu>
+
+            <Menu style={{ float: 'right' }} defaultIndex={0} mode="vertical" onSelect={(e) => console.log(e)}>
                 <MenuItem index={0}>菜单1</MenuItem>
                 <MenuItem index={1}>菜单2</MenuItem>
                 <MenuItem index={2}>菜单3</MenuItem>
