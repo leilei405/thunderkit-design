d2c5e6d35f0bdf2985c7db83c05f8c2e2e451477
fll <fllning@163.com>
2023-10-15 01:35:21 +0800

    fix: Aoo.test.tsx 报错解决

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,16 @@ const App = () => {
       <header className="App-header">
         <ButtonUseCom />
         <MenuUseCom />
+       
       </header>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
     </div>
   );
 }
