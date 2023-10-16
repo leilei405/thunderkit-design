2a1b5cf70d3e75bc26a2f398775fcaf3c49120af
fll <fllning@163.com>
2023-10-17 00:37:10 +0800

    feat: eslint

--- src/components/Icon/types.ts
+++ src/components/Icon/types.ts
@@ -1,7 +1,8 @@
 import React from 'react';
+
 import { FontAwesomeIconProps } from '@fortawesome/react-fontawesome';
-export type ThemeProps = 'primary' | 'default' | 'danger' | 'link' | 'secondary' | 
-'success' |  'info' | 'warning' | 'light' | 'dark'
+
+export type ThemeProps = 'primary' | 'default' | 'danger' | 'link' | 'secondary' | 'success' |  'info' | 'warning' | 'light' | 'dark'
 
 export interface IconProps extends FontAwesomeIconProps {
     theme?: ThemeProps
